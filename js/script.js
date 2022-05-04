fde9ae027f1d7c9432e0d6fcecf76918c774d5ba
Warrengers74 <warrengers.dev@gmail.com>
2022-05-04 16:14:23 +0200

    verification des champs + confirmation denvoi

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let submitForm = document.getElementById("form");
+let alert = document.getElementById("alert");
 
 submitForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -8,5 +9,27 @@ submitForm.addEventListener("submit", (e) => {
     body: form,
   });
   document.dataForm.reset()
+  alert.innerHTML = "**Your message has been sent**"
 });
 
+let email = document.getElementById("mail");
+let regexMail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/;
+
+email.addEventListener("keyup", () => {
+  if(email.value.match(regexMail)) {
+    email.setCustomValidity("");
+  } else {
+    email.setCustomValidity("Please enter a valid email");
+  }
+});
+
+let fullname = document.getElementById("name");
+let regexName = /^[a-zA-ZÜ-ü-' ]{2,50}$/;
+
+fullname.addEventListener("keyup", () => {
+  if(fullname.value.match(regexName)) {
+    fullname.setCustomValidity("");
+  } else {
+    fullname.setCustomValidity("Between 2 and 50 characters. Some characters are not allowed");
+  }
+});
